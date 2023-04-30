7df06ac5b58d42e429bf43d08a87bc410cd8ee43
IvanAvlasevich <avlasevich_ij@yahoo.com>
2023-04-30 15:59:02 +0300

    feat: add textarea.js

---
+++ textarea.js
@@ -0,0 +1,10 @@
+//! -------------------- Отрисовка текст ареи---------------
+const textAreaRender = () => {
+  const textArea = document.createElement('textarea');
+  textArea.className = 'text-area';
+  textArea.setAttribute('rows', 10);
+  textArea.setAttribute('cols', 100);
+  // textArea.autofocus = 'true';
+  document.querySelector('body').append(textArea);
+};
+export { textAreaRender };
